44b1afe5f1ebedde2c26db7f68610f15cdb7b325
mateuszmatyska <mateuszmatyska@gmail.com>
2021-04-20 00:02:05 +0200

    wrap rest api

--- App.js
+++ App.js
@@ -4,11 +4,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import ApplicationStack from "./src/navigation/stackNav";
 import { navigationRef } from "./src/navigation/navService";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { RestLink } from 'apollo-link-rest';
+
+const restLink = new RestLink({ uri: "https://swapi.dev/api/" });
 
 const client = new ApolloClient({
-  uri: 'https://api.graphql.guide/graphql',
   cache: new InMemoryCache(),
+  link: restLink
 });
+
+// const client = new ApolloClient({
+//   uri: 'https://api.graphql.guide/graphql',
+//   cache: new InMemoryCache(),
+// });
 
 export default function App() {
   return (
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -3,18 +3,29 @@ import { SafeAreaView, Text} from 'react-native'
 import { gql, useQuery } from '@apollo/client'
 import {styles} from './styles'
 
-const CHAPTERS_QUERY = gql`
-  query Chapters {
-    chapters {
-      id
-      number
-      title
+// const CHAPTERS_QUERY = gql`
+//   query Chapters {
+//     chapters {
+//       id
+//       number
+//       title
+//     }
+//   }
+// `
+
+const query = gql`
+  query Luke {
+    person @rest(type: "Person", path: "people/1/") {
+      name
     }
   }
-`
+`;
 
 const HomeScreen = () => {
-    const { data, loading } = useQuery(CHAPTERS_QUERY)
+    // const { data, loading } = useQuery(CHAPTERS_QUERY)
+    // console.log(data);
+
+    const { data, loading } = useQuery(query)
     console.log(data);
 
     return <SafeAreaView style={styles.container}>
